2386a297ac2880267a7d2f1d7603bdc25f66f86b
TiChuot97 <daohoanglan2341997@gmail.com>
2019-05-01 18:18:01 -0500

    Shorten 2SAT

--- Graph Theory/two_sat.cpp
+++ Graph Theory/two_sat.cpp
@@ -1,5 +1,5 @@
 // Tested with: http://www.spoj.com/problems/TORNJEVI/
-// Note: for tarjan() and topo_sort(), see tarjan.cpp and topo_sort.cpp
+// Note: for tarjan() see tarjan.cpp
 
 bool two_sat() {
     for (int i = 0; i < list_node.size(); ++i)
@@ -7,18 +7,7 @@ bool two_sat() {
     for (int i = 0; i < list_node.size(); ++i) {
         int u = list_node[i];
         if (comp[u] == comp[neg[u]]) return false;
-        for (int j = 0; j < adj[u].size(); ++j) {
-            int v = adj[u][j];
-            if (comp[u] == comp[v]) continue;
-            new_adj[comp[u]].push_back(comp[v]);
-            ++deg[comp[v]];
-        }
-    }
-    topo_sort();
-    for (int i = 0; i < list_node.size(); ++i) {
-        int u = list_node[i];
-        // position[u]: position of u after topo sorted
-        if (position[comp[u]] > position[comp[neg[u]]])
+        if (comp[u] < comp[neg[u]])
             check[u] = 1; // Pick u (otherwise pick !u)
     }
     return true;
